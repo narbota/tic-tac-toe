a809f8eff4f5c1f2b918db2357fac3bf1901c0d2
Andrea N <andrea@narbot.com>
2021-10-18 12:37:35 -0400

    update app.css

--- src/components/Square.js
+++ src/components/Square.js
@@ -3,13 +3,13 @@ import React from 'react'
 function Square({value, onClick}) {
  
  return (
-  <div>
+  <>
    <button className="square" 
    
    onClick={onClick}
    disabled={value}
    >{value}</button>
-  </div>
+  </>
  )
 }
 
